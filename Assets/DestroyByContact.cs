c9f8814bf005938002b44db10c4dd25c0d3caeb9
NataliaZhidikina <nataliazhideikina@gmail.com>
2020-02-13 10:04:39 +0200

    full project

---
+++ Assets/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+		if(col.CompareTag("Bullet"))
+			{
+			Destroy (col.gameObject);
+			Destroy (gameObject);
+			}
+}
+}
---
+++ Assets/RandomRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour {
+
+	public float tumble;
+	public Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.angularVelocity = new Vector3(1,1,1)*tumble;
+
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+
+}
